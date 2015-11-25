b18c8c9c08f7d9c4cfa43b9eb789c8d749b5b449
rona <rona.lin@elico-corp.com>
2015-11-25 12:20:16 +0800

    [IMP]10816 add module anglo_saxon_account_pos.

---
+++ anglo_saxon_account_pos/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
+#    Alex Duan <alex.duan@elico-corp.com>
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'POS Account Improvement',
+ 'version': '1.0',
+ 'category': 'Generic Modules',
+ 'depends': ['account', 'point_of_sale', 'sale'],
+ 'author': 'Elico Corp',
+ 'license': 'AGPL-3',
+ 'website': 'https://www.elico-corp.com',
+ 'description': """
+POS Account improvement:
+    * group by pos session for bank statement.
+""",
+ 'images': [],
+ 'demo': [],
+ 'data': ['account_view.xml'],
+ 'installable': True,
+ 'application': False,
+ }
+ 
