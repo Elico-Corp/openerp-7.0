e5be753b8abb9df8e44fe455c65ea7bec37ef074
rona <rona.lin@elico-corp.com>
2015-11-28 13:20:42 +0800

    [IMP]11055 add module base_partner_translation.

---
+++ base_partner_translation/base_partner_translation.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2011 Elico Corp. All Rights Reserved.
+#    Author:            Eric CAUDAL <contact@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, orm
+
+
+class res_company(orm.Model):
+    _description = 'Companies'
+    _inherit = "res.company"
+    _name = "res.company"
+    _columns = {
+        'name': fields.char(
+            'Company Name', size=64, required=True, translate=True),
+    }
+
+
+class res_partner(orm.Model):
+    _description = 'Partner'
+    _name = "res.partner"
+    _inherit = "res.partner"
+    _columns = {
+        'name': fields.char('Name', size=128, required=True, select=True,
+                            translate=True),
+    }
+
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        if not context:
+            context = {}
+        if name:
+            # Be sure name_search is symetric to name_get
+            name = name.split(' / ')[-1]
+            ids = self.search(cr, uid, [('name', operator, name)] + args, limit=limit, context=context)
+        else:
+            ids = self.search(cr, uid, args, limit=limit, context=context)
+        return self.name_get(cr, uid, ids, context)
