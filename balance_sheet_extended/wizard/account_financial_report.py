1fcb185c62c6d02f9719517bd16202b1a6b804c1
rona <rona.lin@elico-corp.com>
2015-11-28 14:58:49 +0800

    [IMP]11076 add module balance_sheet_extended.

---
+++ balance_sheet_extended/wizard/account_financial_report.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+class accounting_report(osv.osv_memory):
+    _inherit = "accounting.report"
+    def _print_report(self, cr, uid, ids, data, context=None):
+        data['form'].update(self.read(cr, uid, ids, ['date_from_cmp',  'debit_credit', 'date_to_cmp',  'fiscalyear_id_cmp', 'period_from_cmp', 'period_to_cmp',  'filter_cmp', 'account_report_id', 'enable_filter', 'label_filter','target_move'], context=context)[0])
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'account.financial.report.extended',
+            'datas': data,
+        }
+
+accounting_report()
