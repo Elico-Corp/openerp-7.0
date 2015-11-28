b0d033a6251f28fb626b508b82e8fbdd3b9226b0
rona <rona.lin@elico-corp.com>
2015-11-28 13:53:36 +0800

    [IMP]11061 add module product_price_search.

---
+++ product_price_search/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
+#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Product Price Category',
+    'version': '1.4',
+    'author': 'Elico Corp',
+    'website': 'http://www.elico-corp.com',
+    'summary': '',
+    'description' : """
+    User can quickly search the product price base on the quality.
+    """,
+    'depends': ['base','product',],
+    'category': '',
+    'sequence': 16,
+    'demo': [],
+    'data': [
+             'security/price_search_security.xml',
+             'security/ir.model.access.csv',
+             'product_price.xml',
+        ],
+    'test': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'css': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
