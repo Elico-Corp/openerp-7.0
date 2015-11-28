af2c1485bfb2c639202bca59e64bcdc4d2596495
rona <rona.lin@elico-corp.com>
2015-11-28 17:37:51 +0800

    [IMP]11084 add module stock_incoming_shippment_container.

---
+++ stock_incoming_shippment_container/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
+#    Alex Duan <alex.duan@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'stock_incoming_shippment_container',
+ 'version': '1.0',
+ 'category': 'Generic Modules',
+ 'depends': ['stock'],
+ 'author': 'Elico Corp',
+ 'license': 'AGPL-3',
+ 'website': 'https://www.elico-corp.com',
+ 'description': """
+ Incoming shipments -> Page, add a new field called: Container Number
+And show Container # on the IS list page as a column
+""",
+ 'images': [],
+ 'demo': [],
+ 'data': ['stock_view.xml'],
+ 'installable': True,
+ 'application': False,
+ }
