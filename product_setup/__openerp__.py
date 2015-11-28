5416b52d87c33870256655a7e0a3c1226101585d
rona <rona.lin@elico-corp.com>
2015-11-28 10:58:02 +0800

    [IMp]10812 add module product_setup.

---
+++ product_setup/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
+#    Bob Lucas <luo.yumeng@elico-corp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Product_setup',
+ 'version': '0.1',
+ 'category': '',
+ 'depends': ['product', 'account', 'product_kits_inventory'],
+ 'author': 'Elico Corp',
+ 'license': 'AGPL-3',
+ 'website': 'https://www.elico-corp.com',
+ 'description': """
+Add several fields to the CRM form
+""",
+ 'images': [],
+ 'demo': [],
+ 'data': ['product_setup_view.xml', 'product_setup.xml'],
+ 'installable': True,
+ 'application': False}
