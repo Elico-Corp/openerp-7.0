1e1b77cdd45b633902c621c5cb6b6b04479583e2
kevin <kevin.huang@elico-corp.com>
2017-03-01 22:47:22 -0800

    modify

--- customer_supplier_default/__openerp__.py
+++ customer_supplier_default/__openerp__.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
-#    Author: LIN Yu <lin.yu@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 {
-    "name" : "Un-select customer or supplier by default when creating contacts for companies.",
-    "version" : "1.0",
+    "name" : "customer supplier default",
+    "version" : "7.0.1.0.0",
     "author" : "Elico Corp",
     "website" : "http://www.openerp.net.cn",
     "description": """
@@ -31,7 +13,6 @@
     """,
     "depends" : ['base','sale_crm'],
     "category" : "Sales",
-    "demo_xml" : [],
     "update_xml" : ['customer_supplier_view.xml'],
     "license": "AGPL-3",
     "active": False,
