f9691eccf9999bd52cdc94febf6630df22c1acbf
kevin <kevin.huang@elico-corp.com>
2017-03-01 22:56:42 -0800

    modify

--- customer_supplier_default/__init__.py
+++ customer_supplier_default/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2014 Elico corp(www.elico-corp.com)
 # Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
-from openerp.osv import orm
 
+import customer_supplier_default
 
-class event_company_def(orm.Model):
-
-    _inherit = 'event.event'
-
-    _defaults = {'company_id': ''}
