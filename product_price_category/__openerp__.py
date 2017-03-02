3217c73a433b602d999db9ed5fe5e8c2ba9df72a
kevin <kevin.huang@elico-corp.com>
2017-03-01 18:56:00 -0800

    modify

--- product_price_category/__openerp__.py
+++ product_price_category/__openerp__.py
@@ -1,51 +1,28 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
-#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
 {
     'name': 'Product Price Category',
-    'version': '1.4',
+    'version': '7.0.1.0.0',
     'author': 'Elico Corp',
     'website': 'http://www.elico-corp.com',
-    'summary': '',
     'description' : """
      Display the price category for products.
     """,
     'depends': ['base','product', 'sale','purchase'],
-    'category': '',
     'sequence': 16,
-    'demo': [],
     'data': [      
         'price_category.xml',
         'security/ir.model.access.csv',
-        # data record use some eval=id, should change to ref xml_id
         'pricelist.xml',
         'product_view.xml',
         ],
-    'test': [],
     'installable': True,
     'application': False,
     'auto_install': False,
-    'css': [],
+
 }
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
 
