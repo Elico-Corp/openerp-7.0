40606fd491d86cacc9232d3e419a179557f02586
kevin <kevin.huang@elico-corp.com>
2017-03-05 22:58:13 -0800

    modify

--- sale_sample/sale.py
+++ sale_sample/sale.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
-#    Author: Qiao Lei <qiao.lei@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or Later(http://www.gnu.org/licenses/agpl.html)
+
 
 
 import time
@@ -37,22 +20,6 @@ class sale_order_line(osv.osv):
     }
     #move this button to modeul "sale_botton_price"
     #if not need button price 
-#===============================================================================
-#     def product_id_change___(self, cr, uid, ids, sample, pricelist, product, qty=0,
-#             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
-#             lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
-#         
-#         res = super(sale_order_line,self).product_id_change( cr, uid, ids, pricelist, product, qty=qty,
-#             uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
-#             lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, 
-#             fiscal_position=fiscal_position, flag=flag, context=context)
-# 
-#         if sample:
-#             res['value'].update({'price_unit':0})
-#             
-#         return res
-#===============================================================================
-
     def product_uom_change(self, cursor, user, ids, sample, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, context=None):
