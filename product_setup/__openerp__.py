62a1d3b2a5d8535469564150e9f0210362388c22
kevin <kevin.huang@elico-corp.com>
2017-03-05 23:56:57 -0800

    modify

--- product_setup/__openerp__.py
+++ product_setup/__openerp__.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
-#    Bob Lucas <luo.yumeng@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or Later(http://www.gnu.org/licenses/agpl.html)
+
 {'name': 'Product_setup',
- 'version': '0.1',
- 'category': '',
+ 'version': '7.0.1.0.0',
  'depends': ['product', 'account', 'product_kits_inventory'],
  'author': 'Elico Corp',
  'license': 'AGPL-3',
@@ -29,8 +11,6 @@
  'description': """
 Add several fields to the CRM form
 """,
- 'images': [],
- 'demo': [],
  'data': ['product_setup_view.xml', 'product_setup.xml'],
  'installable': True,
  'application': False}
