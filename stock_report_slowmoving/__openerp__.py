fec9e9221c34a1e658924bd8ee4601fffc253878
kevin <kevin.huang@elico-corp.com>
2017-03-08 22:28:26 -0800

    modify

--- stock_report_slowmoving/__openerp__.py
+++ stock_report_slowmoving/__openerp__.py
@@ -1,30 +1,12 @@
 #-*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution    
-#    Copyright (c) 2010-today Elico Corp. All Rights Reserved.
-#    Author:            Andy Lu <andy.lu@elico-corp.com>
-#    $Id$
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
+
 
 
 {
-    'name': 'Slow moving stock report in a night scheduler',
-    'version': '1.0',
+    'name': 'Stock report slow-moving',
+    'version': '7.0.1.0.0',
     'category': 'Custom',
     'description': """
 
@@ -40,16 +22,12 @@
     'depends': ['base', 'product', 'stock', 'sale', 'purchase', 'mrp',
         'report_webkit',
         'base_headers_webkit'],
-    'init_xml': [],
     'update_xml': [
         'security/ir.model.access.csv',
         'stock_slowmoving_view.xml',
         'stock_slowmoving_report_view.xml',
         'wizard/stock_slowing_move_view.xml',
     ],
-    'demo_xml': [], 
-    'test': [],
     'installable': True,
     'active': False,
-    'certificate': '',
 }
