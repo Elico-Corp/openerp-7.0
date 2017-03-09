558ec608f1ee3692f69d985852d1abdfbcb14fcb
kevin <kevin.huang@elico-corp.com>
2017-03-08 18:36:02 -0800

    modify

--- mrp_to_produce_editable/__openerp__.py
+++ mrp_to_produce_editable/__openerp__.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
-#    Alex Duan <alex.duan@elico-corp.com>
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-{'name': 'mrp_to_product_editable',
- 'version': '1.0',
+{'name': 'Mrp to product editable',
+ 'version': '7.0.1.0.0',
  'category': 'Generic Modules',
  'depends': ['mrp', 'stock', 'mrp_mo_split'],
  'author': 'Elico Corp',
@@ -30,8 +13,6 @@
  'description': """
  This module allows the quantity of to producing products editable on MO
 """,
- 'images': [],
- 'demo': [],
  'data': ['mrp_view.xml', 'mrp_workflow.xml'],
  'installable': True,
  'application': False,
