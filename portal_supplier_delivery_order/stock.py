c564f18a9c9b5b4ee6d5326e2f5c789bf3a09fe6
kevin <kevin.huang@elico-corp.com>
2017-03-08 22:55:26 -0800

    modify

--- portal_supplier_delivery_order/stock.py
+++ portal_supplier_delivery_order/stock.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
-#    LIN Yu <lin.yu@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
+
 from openerp.osv import orm, fields
 
 
@@ -29,16 +12,6 @@ class stock_picking(orm.Model):
         'sales_man': fields.many2one(
             'res.users', 'Source product', ondelete='cascade'),
     }
-
-    #back order, copy should copy sales man
-    # def copy_data(self, cr, uid, id, default=None, context=None):
-    #     if default is None:
-    #         default = {}
-    #     default['sales_man'] = False
-    #     return super(sale_order, self).copy_data(cr, uid, id,
-    #                                              default=default,
-    #                                              context=context)
-
 
 class stock_picking_out(orm.Model):
     _inherit = 'stock.picking.out'
