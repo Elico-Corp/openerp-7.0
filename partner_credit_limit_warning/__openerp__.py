11b28964174e8da3a323f94b8b2cd022baef7f3a
kevin <kevin.huang@elico-corp.com>
2017-03-08 18:47:53 -0800

    modify

--- partner_credit_limit_warning/__openerp__.py
+++ partner_credit_limit_warning/__openerp__.py
@@ -1,30 +1,11 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution    
-#    Copyright (c) 2010-today Elico Corp. All Rights Reserved.
-#    Author:            Andy Lu <andy.lu@elico-corp.com>
-#    $Id$
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
 
 {
     'name': 'Check Partner credit limit',
-    'version': '1.0',
+    'version': '7.0.1.0.0',
     'category': 'Sales',
     'description': """
         Check the credit limit of the customer and:
@@ -35,14 +16,10 @@
     'author': 'Elico Corp',
     'website': 'http://www.openerp.net.cn',
     'depends': ['sale', 'sale_quotation', 'stock'],
-    'init_xml': [],
     'update_xml': [
         'stock_view.xml',
         'wizard/sale_confirm_warning.xml',
     ],
-    'demo_xml': [], 
-    'test': [],
     'installable': True,
     'active': False,
-    'certificate': '',
 }
