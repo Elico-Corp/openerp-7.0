8ef08502e6e4af4a1040d97c848b4abf38416075
kevin <kevin.huang@elico-corp.com>
2017-03-09 01:08:58 -0800

    modify

--- stock_delivery_order_tradevine_ref/__openerp__.py
+++ stock_delivery_order_tradevine_ref/__openerp__.py
@@ -1,26 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
-#    Alex Duan <alex.duan@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'stock_delivery_order_tradevine_ref',
- 'version': '1.0',
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'stock delivery order tradevine ref',
+ 'version': '7.0.1.0.0',
  'category': 'Generic Modules',
  'depends': ['stock', 'sale'],
  'author': 'Elico Corp',
@@ -29,8 +12,6 @@
  'description': """
     1- add a new field sale ref from SO.
 """,
- 'images': [],
- 'demo': [],
  'data': ['stock_view.xml'],
  'installable': True,
  'application': False,
