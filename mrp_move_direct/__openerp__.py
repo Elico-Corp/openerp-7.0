0b85926017109f1b7a09c5928da718c9cc1fbb09
Ding DeChuan <1364869063@qq.com>
2017-05-05 13:55:15 +0800

    some modifications about remark

--- mrp_move_direct/__openerp__.py
+++ mrp_move_direct/__openerp__.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
-#    Author: Andy Lu <andy.lu@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
 
 {
     'name': 'MRP support Add or Cancel the moves.',
-    'version': '1.1',
+    'version': '7.0.1.0.0',
     'author': 'Andy Lu',
     'website': 'http://www.openerp.com.cn',
     'category': 'Manufacturing',
@@ -32,13 +14,13 @@
     'images': [],
     'depends': ['mrp','procurement', 'stock'],
     'description': """
-MRP support Add or Cancel the moves in OpenERP
-===========================================
+        MRP support Add or Cancel the moves in OpenERP
+        ===========================================
 
-The module allows you to:
-* Add new products to consume directly.
-* Cancel(Scrap) products to consume directly.
-* recalculate cost
+        The module allows you to:
+        * Add new products to consume directly.
+        * Cancel(Scrap) products to consume directly.
+        * recalculate cost
 
     """,
     'data': [
@@ -50,4 +32,4 @@ The module allows you to:
     'application': False,
     'auto_install': False,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
