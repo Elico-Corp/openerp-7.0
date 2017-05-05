931da1dbbe3f96d489ec3f1096d151ed43189765
Ding DeChuan <1364869063@qq.com>
2017-05-05 16:46:43 +0800

    some modifications about remark

--- stock_with_cost/__openerp__.py
+++ stock_with_cost/__openerp__.py
@@ -1,35 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Elico Corp. All Rights Reserved.
-#    Author: Yannick Gouin <yannick.gouin@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
 {
     'name': 'Stock With Cost',
-    'version': '1.0',
+    'version': '7.0.1.0.0',
     'category': 'Stock',
     'sequence': 19,
     'summary': 'Stock with Cost',
     'description': """
-Stock With Cost
-==================================================
-* Add Cost (POL Price )in Stock Move, Stock picking
+        Stock With Cost
+        ==================================================
+        * Add Cost (POL Price )in Stock Move, Stock picking
     """,
     'author': 'Elico Corp',
     'website': 'http://www.elico-corp.com',
@@ -45,4 +27,4 @@ Stock With Cost
     'application': False,
 }
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
