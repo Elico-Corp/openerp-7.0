22c25cfd9434695effa8437f826a45fde2b98da7
Ding DeChuan <1364869063@qq.com>
2017-05-05 15:59:10 +0800

    some modifications about remark

--- stock_location_reports/report/stock_inventory_move_report.py
+++ stock_location_reports/report/stock_inventory_move_report.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Elico Corp (<http://www.elico-corp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
+
 
 import time
 from openerp.report import report_sxw
@@ -44,4 +28,4 @@ report_sxw.report_sxw(
     parser=stock_inventory_move_elico,
     header='internal'
 )
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
