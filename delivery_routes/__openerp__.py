8cf143b8b432b02f2d0372018a790b31bca1929f
Ding DeChuan <1364869063@qq.com>
2017-05-05 17:49:00 +0800

    add Copyright

--- delivery_routes/__openerp__.py
+++ delivery_routes/__openerp__.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+# © 2011 Cubic ERP - Teradata SAC(http://cubicerp.com)
 # © 2014 Elico corp(www.elico-corp.com)
 # Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
@@ -44,4 +45,4 @@
     "certificate" : "",
     'images': [],
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
