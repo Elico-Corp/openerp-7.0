ebcfcc3bb4e449421f3c79f8502ae10c24c0ec44
Ding DeChuan <1364869063@qq.com>
2017-05-08 14:41:09 +0800

    modify file remarks

--- purchase_landed_costs_extended/__openerp__.py
+++ purchase_landed_costs_extended/__openerp__.py
@@ -1,44 +1,29 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2014 Elico Corp. All Rights Reserved.
-#    Augustin Cisterne-Kaas <augustin.cisterne-kaas@elico-corp.com>
-#    Alex Duan <alex.duan@elico-corp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-{'name': 'Purchase Landed Costs Extended',
- 'version': '0.1',
- 'category': 'Warehouse Management',
- 'depends': ['purchase', 'stock', 'account', 'purchase_landed_costs'],
- 'author': 'Elico Corp',
- 'license': 'AGPL-3',
- 'website': 'https://www.elico-corp.com',
- 'description': """
+# -*- encoding: utf-8 -*-
+# © 2014 Elico corp(www.elico-corp.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
-""",
- 'images': [],
- 'demo': [],
- 'data': ['purchase_view.xml',
-          'report/purchase_report_view.xml',
-          'wizard/landed_cost_position_invoice_view.xml',
-          'wizard/shipment_wizard_view.xml',
-          'security/ir.model.access.csv',
-          'stock_view.xml',
-          'shipment_view.xml',
-          'purchase_landed_costs_extended_data.xml'],
- 'installable': True,
- 'application': False}
+
+{
+	'name': 'Purchase Landed Costs Extended',
+	'version': '7.0.1.0.0',
+	'category': 'Warehouse Management',
+	'depends': ['purchase', 'stock', 'account', 'purchase_landed_costs'],
+	'author': 'Elico Corp',
+	'license': 'AGPL-3',
+	'website': 'https://www.elico-corp.com',
+	'description': """
+
+	""",
+	'images': [],
+	'demo': [],
+	'data': ['purchase_view.xml',
+	  'report/purchase_report_view.xml',
+	  'wizard/landed_cost_position_invoice_view.xml',
+	  'wizard/shipment_wizard_view.xml',
+	  'security/ir.model.access.csv',
+	  'stock_view.xml',
+	  'shipment_view.xml',
+	  'purchase_landed_costs_extended_data.xml'],
+	'installable': True,
+	'application': False
+ }
