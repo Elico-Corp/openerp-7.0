e351a3ce12e4944dfadc9e0661cd6d60b93de693
Ding DeChuan <1364869063@qq.com>
2017-05-08 12:02:34 +0800

    modify file remarks

--- product_multi_price/__openerp__.py
+++ product_multi_price/__openerp__.py
@@ -1,28 +1,12 @@
-# -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-#    product_multi_price for OpenERP                                          #
-#    Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>   #
-#                                                                               #
-#    This program is free software: you can redistribute it and/or modify       #
-#    it under the terms of the GNU Affero General Public License as             #
-#    published by the Free Software Foundation, either version 3 of the         #
-#    License, or (at your option) any later version.                            #
-#                                                                               #
-#    This program is distributed in the hope that it will be useful,            #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-#    GNU Affero General Public License for more details.                        #
-#                                                                               #
-#    You should have received a copy of the GNU Affero General Public License   #
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
-#                                                                               #
-#################################################################################
+# -*- coding: utf-8 -*-
+# © 2014 Elico corp(www.elico-corp.com)
+# © 2014 Akretion Sébastien BEAU (sebastien.beau@akretion.com)
+# Licence AGPL-3.0 or later(http://www.gnu.org/licenses/agpl.html)
 
 
 {
     'name': 'product_multi_price',
-    'version': '6.1.0',
+    'version': '7.0.1.0.0',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'description': """empty""",
